6cc45b16a1217b127be5f46f2a0a8b2572b2ab04
loteque <andrewandroid@gmail.com>
2023-06-09 11:44:12 +0200

    retry quit screen now properly exits and restarts.

--- project/src/ui/PlayerUI.gd
+++ project/src/ui/PlayerUI.gd
@@ -1,12 +1,22 @@
 extends CanvasLayer
 
+export (NodePath) var main_path = ".."
+
+onready var main: Node = get_node(main_path)
 onready var rust_meter_bar: Node = get_node("RustMeterContainer/RustMeter/RustMeterBar")
 
 func _on_Retry_button_down():
-	get_tree().reload_current_scene()
-
+	main.level_manager.unload_scene(main.level_manager.scene_node)
+	main.level_manager.load_scene_by_index(main.level_manager.current_scene_paths_index)
+	main.level_manager.handle_player()
+	main.player_controller.reset()
+	$Rusted.hide()
+	
 func _on_Exit_button_down():
-	get_tree().quit()
+	main.level_manager.unload_scene(main.level_manager.scene_node)
+	main.level_manager.load_scene_by_index(0)
+	main.level_manager.handle_player()
+	$Rusted.hide()
 
 func reset_rust_meter():
 	rust_meter_bar.rect_size.x = 0
